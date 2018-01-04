065fe120493dd2c53dc142a321ea8970f1b3ac33
Paweł Srebrny <psrebrny@one2tribe.pl>
2018-01-04 19:54:30 +0100

    set up production build and server

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const distPatch = path.join(__dirname, '..', 'dist');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(distPatch));
 
 app.get('*', (req, res) => {
-  console.log('get any');
   res.sendFile(path.join(distPatch, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up!');
 });
