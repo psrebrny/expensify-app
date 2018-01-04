944eab3d77fbcfd1262c3b2ae32f6b29fb810145
Paweł Srebrny <psrebrny@one2tribe.pl>
2018-01-04 18:50:09 +0100

    update build methods to dev and prod, prepare webpack config to both environments

--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,60 @@ let path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    historyApiFallback: true,
-    //   contentBase: './public',
-    //   hot: true
-  },
-  module: {
-    // loaders: [{
-    //   test: /\.s?css$/,
-    //   loader: ExtractTextPlugin.extract('style', 'css!sass')
-    // }],
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: [/(node_modules)/]
-      },
-      {
-        test: /\.s?css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
+
+module.exports = (env, argv) => {
+  const isProduction = env === 'production';
+  
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/'
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      historyApiFallback: true,
+      //   contentBase: './public',
+      //   hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: [/(node_modules)/]
+        },
+        {
+          test: /\.s?css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                  minimize: isProduction
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
               }
-            }
-          ],
-        })
-      }
-    ],
-    
-  },
-  plugins: [
-    new ExtractTextPlugin('style.css'),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-  ]
+            ],
+          })
+        }
+      ],
+      
+    },
+    plugins: [
+      new ExtractTextPlugin('style.css'),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
+    ]
+  };
+  
 };
