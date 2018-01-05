d9f262db374d3e43865b860f759419580f716506
Paweł Srebrny <psrebrny@one2tribe.pl>
2018-01-05 23:42:28 +0100

    change checking environment method, remove coverage from git repository

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,61 @@
+let webpack = require('webpack');
 let path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const isProduction = process.env.NODE_ENV === 'production';
 
-module.exports = (env, argv) => {
-  const isProduction = env === 'production';
-  
-  return {
-    entry: './src/app.js',
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'bundle.js',
-      publicPath: '/'
-    },
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
-    devServer: {
-      historyApiFallback: true,
-      //   contentBase: './public',
-      //   hot: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: 'babel-loader',
-          exclude: [/(node_modules)/]
-        },
-        {
-          test: /\.s?css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  sourceMap: true,
-                  minimize: isProduction
-                }
-              },
-              {
-                loader: 'sass-loader',
-                options: {
-                  sourceMap: true
-                }
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
+  devServer: {
+    historyApiFallback: true
+    //   contentBase: './public',
+    // hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: [/(node_modules)/]
+      },
+      {
+        test: /\.s?css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                minimize: isProduction
               }
-            ],
-          })
-        }
-      ],
-      
-    },
-    plugins: [
-      new ExtractTextPlugin('style.css'),
-      new HtmlWebpackPlugin({
-        template: 'src/index.html',
-      }),
-    ]
-  };
-  
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+        })
+      }
+    ],
+    
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify((process.env.NODE_ENV))
+    })
+  ]
 };
